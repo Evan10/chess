e4578e993460346ac91284400fdb04418993691e
Evan <evanroyal1@gmail.com>
2026-02-18 12:56:49 -0700

    Implemented basic handler classes

--- server/src/main/java/handler/ChessGameHandler.java
+++ server/src/main/java/handler/ChessGameHandler.java
@@ -1,4 +1,46 @@
 package handler;
 
+import requestResult.*;
+
+import service.GameService;
+
 public class ChessGameHandler {
+
+    private final GameService gameService;
+
+    private final JsonToRequestConverter<JoinGameRequest> joinDeserializer;
+    private final JsonToRequestConverter<ListGamesRequest> listDeserializer;
+    private final JsonToRequestConverter<CreateGameRequest> createDeserializer;
+
+    private final ResultToJsonStringConverter serializer;
+
+    public ChessGameHandler(GameService gameService){
+        this.gameService=gameService;
+
+        joinDeserializer = new JsonToRequestConverter<>(JoinGameRequest.class);
+        listDeserializer = new JsonToRequestConverter<>(ListGamesRequest.class);
+        createDeserializer = new JsonToRequestConverter<>(CreateGameRequest.class);
+
+        serializer = ResultToJsonStringConverter.getInstance();
+    }
+
+    public String joinGameHandler(String json){
+        JoinGameRequest req = joinDeserializer.convert(json);
+        JoinGameResult res = gameService.joinGame(req);
+        return serializer.resToString(res);
+    }
+
+    public String listGamesHandler(String json){
+        ListGamesRequest req = listDeserializer.convert(json);
+        ListGamesResult res = gameService.listGames(req);
+        return serializer.resToString(res);
+    }
+
+    public String createGameHandler(String json){
+        CreateGameRequest req = createDeserializer.convert(json);
+        CreateGameResult res = gameService.createGame(req);
+        return serializer.resToString(res);
+    }
+
+
 }
--- server/src/main/java/handler/UserHandler.java
+++ server/src/main/java/handler/UserHandler.java
@@ -1,4 +1,45 @@
 package handler;
 
+import requestResult.*;
+
+import service.UserService;
+
 public class UserHandler {
+
+    private final UserService userService;
+
+    private final JsonToRequestConverter<RegisterRequest> registerDeserializer;
+    private final JsonToRequestConverter<LoginRequest> loginDeserializer;
+    private final JsonToRequestConverter<LogoutRequest> logoutDeserializer;
+
+    private final ResultToJsonStringConverter serializer;
+
+    public UserHandler(UserService userService){
+        this.userService=userService;
+
+        registerDeserializer = new JsonToRequestConverter<>(RegisterRequest.class);
+        loginDeserializer = new JsonToRequestConverter<>(LoginRequest.class);
+        logoutDeserializer = new JsonToRequestConverter<>(LogoutRequest.class);
+
+        serializer = ResultToJsonStringConverter.getInstance();
+    }
+
+    public String registerHandler(String json){
+        RegisterRequest req = registerDeserializer.convert(json);
+        RegisterResult res = userService.register(req);
+        return serializer.resToString(res);
+    }
+
+    public String loginHandler(String json){
+        LoginRequest req = loginDeserializer.convert(json);
+        LoginResult res = userService.login(req);
+        return serializer.resToString(res);
+    }
+
+    public String logoutHandler(String json){
+        LogoutRequest req = logoutDeserializer.convert(json);
+        LogoutResult res = userService.logout(req);
+        return serializer.resToString(res);
+    }
+
 }
