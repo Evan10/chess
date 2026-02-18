72e07db51728207937abd5aa4e0a2452f822b263
Evan <evanroyal1@gmail.com>
2026-02-18 10:08:08 -0700

    Created needed classes and records

---
+++ server/src/main/java/util/Util.java
@@ -0,0 +1,10 @@
+package util;
+import java.util.UUID;
+
+public class Util {
+
+    public static String newUUID(){
+        return UUID.randomUUID().toString();
+    }
+
+}
---
+++ shared/src/main/java/model/GameData.java
@@ -0,0 +1,8 @@
+package model;
+
+import chess.ChessGame;
+
+public record GameData(String gameID, String whiteUsername,
+                       String blackUsername, String gameName,
+                       ChessGame game) {
+}
