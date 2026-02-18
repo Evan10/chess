f663848fabe9f8e0c9ee2d8ba5011551c6b644b1
Evan <evanroyal1@gmail.com>
2026-02-17 18:19:54 -0700

    Prepared project for phase 3

---
+++ server/src/main/java/ServerMain.java
@@ -0,0 +1,13 @@
+import chess.*;
+import server.Server;
+
+public class ServerMain {
+    public static void main(String[] args) {
+
+        Server server = new Server();
+        int port = 8080;
+        port = server.run(port);
+
+        System.out.println("♕ 240 Chess Server running on: "+port );
+    }
+}
