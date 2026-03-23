898b9a86ac27b75efebeda82bc2a982eb7e40000
Evan <evanroyal1@gmail.com>
2026-03-23 10:59:52 -0600

    Initiallized client code

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,10 +1,46 @@
 package client;
 
 import chess.*;
+import client.dataaccess.HTTPConnection;
+
+import java.util.Scanner;
 
 public class ClientMain {
+
+    private boolean running = false;
+    HTTPConnection serverConnection;
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+
+
     }
+
+    ClientMain(){
+        serverConnection = new HTTPConnection();
+        running = true;
+        run();
+
+    }
+
+
+    private void run(){
+
+        Scanner input = new Scanner(System.in);
+        String value = "";
+        while(running){
+            value = input.nextLine();
+            System.out.printf(getFormattedResponse(value));
+        }
+    }
+
+    private String getFormattedResponse(String message){
+
+
+        return "unknown Input try again";
+
+    }
+
+
+
+
+
 }
---
+++ client/src/main/java/client/dataaccess/HTTPConnection.java
@@ -0,0 +1,29 @@
+package client.dataaccess;
+
+import java.net.http.HttpClient;
+import java.util.Locale;
+
+public class HTTPConnection{
+
+
+    private static final HttpClient client = HttpClient.newBuilder().build();
+
+    private String host;
+    private int port;
+    private String url;
+
+    HTTPConnection(String host, int port){
+        this.host = host;
+        this.port = port;
+        url = String.format(Locale.getDefault(),"http://%s:%d",host,port);
+    }
+
+
+
+
+
+    public void close(){
+        client.close();
+    }
+
+}
